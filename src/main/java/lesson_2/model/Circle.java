eab611e69b67c001d246bbb233819591836fd911
chris <ctschubel@googlemail.com>
2019-01-09 19:51:27 +0100

    initnal Commit

---
+++ src/main/java/lesson_2/model/Circle.java
@@ -0,0 +1,30 @@
+package lesson_2.model;
+
+/**
+ * @author ctschubel
+ *
+ */
+public class Circle extends Shape {
+
+	private double radius;
+
+	public Circle(final double radius) {
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return this.radius;
+	}
+
+	public void setRadius(final double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+
+		return this.radius * this.radius * Math.PI;
+	}
+
+
+}
